508ce823b746ca8295919e4e925890106594890d
Projektorkraft <projektorkraft@gmail.com>
2019-03-03 18:58:05 +0100

    more github pages stuff

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Version 0.0.1",
+        "excerpt":"First public release.   ","categories": ["General"],
+        "tags": ["Publish"],
+        "url": "http://localhost:4000/general/Version001/",
+        "teaser":null}]
