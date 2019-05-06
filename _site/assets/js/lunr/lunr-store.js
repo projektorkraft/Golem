fc40a3bf650f15e0c536489bb3326f9aaeaf5809
Projektorkraft <projektorkraft@gmail.com>
2019-05-06 18:32:12 +0200

    Version010

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
         "title": "Version 0.0.1",
-        "excerpt":"First public release.   ","categories": ["General"],
+        "excerpt":"First public release.   ","categories": ["Golem"],
         "tags": ["Publish"],
-        "url": "http://localhost:4000/general/Version001/",
+        "url": "http://localhost:4000/Golem/golem/Version001/",
+        "teaser":null},{
+        "title": "Version 0.1.0",
+        "excerpt":"We finished our first milestone.  ","categories": [],
+        "tags": ["Publish"],
+        "url": "http://localhost:4000/Golem/Version010/",
         "teaser":null}]
